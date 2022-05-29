adcd9c198c97a77652a2037a38a5b9acd7cb30c4
davitlabadze <labadzedato18@gmail.com>
2022-05-29 15:31:11 +0400

    feat: db seeder

---
+++ backend/data/products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        name: "MacBook Pro 13-inch",
+        description:"MacBook Pro 13-inch",
+        price:1299,
+        countInStock:12,
+        imageUrl:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000",
+    },
+    {
+        name: "MacBook Pro 14-inch",
+        description:"MacBook Pro 14-inch",
+        price:2299,
+        countInStock:15,
+        imageUrl:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000",
+    },
+    {
+        name: "MacBook Pro 15-inch",
+        description:"MacBook Pro 15-inch",
+        price:3299,
+        countInStock:15,
+        imageUrl:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000",
+    },
+];
+
+module.exports = products;
