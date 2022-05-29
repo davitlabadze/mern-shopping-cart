ba6c2e94c58fd10da45f847643e0ad78e6fba11a
davitlabadze <labadzedato18@gmail.com>
2022-05-29 23:13:16 +0400

    style:home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,29 @@
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className="z-20 grid grid-flow-col gap-10 py-4 overflow-x-auto text-center ">
+            <div className="duration-300 scale-95 border border-gray-100 rounded-lg shadow-lg hover:scale-100 w-80 h-96 hover:shadow-xl">
+                <h1 className="mt-2 text-2xl">MacBook Pro 13"</h1>
+                <img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000" alt="" />
+                <button className="p-4 text-white bg-blue-600 rounded-md hover:bg-blue-500">Add to Cart</button>
+            </div>
+            <div className="duration-300 scale-95 border border-gray-100 rounded-lg shadow-lg hover:scale-100 w-80 h-96 hover:shadow-xl">
+                <h1 className="mt-2 text-2xl">MacBook Pro 13"</h1>
+                <img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000" alt="" />
+                <button className="p-4 text-white bg-blue-600 rounded-md hover:bg-blue-500">Add to Cart</button>
+            </div>
+            <div className="duration-300 scale-95 border border-gray-100 rounded-lg shadow-lg hover:scale-100 w-80 h-96 hover:shadow-xl">
+                <h1 className="mt-2 text-2xl">MacBook Pro 13"</h1>
+                <img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000" alt="" />
+                <button className="p-4 text-white bg-blue-600 rounded-md hover:bg-blue-500">Add to Cart</button>
+            </div>
+            <div className="duration-300 scale-95 border border-gray-100 rounded-lg shadow-lg hover:scale-100 w-80 h-96 hover:shadow-xl">
+                <h1 className="mt-2 text-2xl">MacBook Pro 13"</h1>
+                <img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000" alt="" />
+                <button className="p-4 text-white bg-blue-600 rounded-md hover:bg-blue-500">Add to Cart</button>
+            </div>
+
+
+        </div>
     )
 }
 
