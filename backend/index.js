e9c6c347ceb52ad6b02ff3bd2588a69a5b3e3423
davitlabadze <labadzedato18@gmail.com>
2022-05-29 14:17:11 +0400

    feat: background structure and connect mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
-const req = require('express/lib/request');
-const app = express()
+const connectDB = require("./config/db");
+connectDB();
+const app = express();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server runing on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server runing on port ${PORT}`));
