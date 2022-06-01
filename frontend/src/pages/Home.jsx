7014ddff663eb5ba1ab66d6d84d508fba4a28560
davitlabadze <labadzedato18@gmail.com>
2022-06-02 00:55:55 +0400

    feat: get product from db

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,27 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
 import Product from "../components/Product"
+import { getProducts as listProducts } from '../redux/actions/productActions';
 
 const Home = () => {
+
+    const dispatch = useDispatch();
+    const getProducts = useSelector((state) => state.getProduct);
+    const { products = [], loading, error } = getProducts;
+
+    useEffect(() => {
+        dispatch(listProducts())
+    }, [dispatch])
+
     return (
         <div className="grid grid-cols-1 gap-10 py-4 text-center lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2">
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
+            {loading ? (
+                <h2>Loading...</h2>
+            ) : error ? (
+                <h2>{error}</h2>
+            ) : (
+                products.map((product) => <Product key={product._id} />)
+            )}
         </div>
     )
 }
