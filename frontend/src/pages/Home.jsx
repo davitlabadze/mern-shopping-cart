8ee880eb70e522f5479f9e4b3b6065dc142b695f
davitlabadze <labadzedato18@gmail.com>
2022-06-01 20:00:16 +0400

    feat: component product

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,49 +1,13 @@
+import Product from "../components/Product"
+
 const Home = () => {
     return (
-        <div className="grid grid-cols-1 gap-10 py-4 text-center  lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2">
-            <div className="duration-300 scale-95 border border-gray-100 rounded-lg shadow-lg hover:scale-100 w-80 h-96 hover:shadow-xl">
-                <h1 className="mt-2 text-2xl">MacBook Pro 13"</h1>
-                <img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000" alt="" />
-                <div className="flex items-center justify-between px-12">
-                    <h4>$5555</h4>
-                    <button className="p-2 text-white bg-blue-600 rounded-md hover:bg-blue-500">Add to Cart</button>
-                </div>
-            </div>
-            <div className="duration-300 scale-95 border border-gray-100 rounded-lg shadow-lg hover:scale-100 w-80 h-96 hover:shadow-xl">
-                <h1 className="mt-2 text-2xl">MacBook Pro 13"</h1>
-                <img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000" alt="" />
-                <div className="flex items-center justify-between px-12">
-                    <h4>$5555</h4>
-                    <button className="p-2 text-white bg-blue-600 rounded-md hover:bg-blue-500">Add to Cart</button>
-                </div>
-            </div>
-            <div className="duration-300 scale-95 border border-gray-100 rounded-lg shadow-lg hover:scale-100 w-80 h-96 hover:shadow-xl">
-                <h1 className="mt-2 text-2xl">MacBook Pro 13"</h1>
-                <img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000" alt="" />
-                <div className="flex items-center justify-between px-12">
-                    <h4>$5555</h4>
-                    <button className="p-2 text-white bg-blue-600 rounded-md hover:bg-blue-500">Add to Cart</button>
-                </div>
-            </div>
-            <div className="duration-300 scale-95 border border-gray-100 rounded-lg shadow-lg hover:scale-100 w-80 h-96 hover:shadow-xl">
-                <h1 className="mt-2 text-2xl">MacBook Pro 13"</h1>
-                <img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000" alt="" />
-                <div className="flex items-center justify-between px-12">
-                    <h4>$5555</h4>
-                    <button className="p-2 text-white bg-blue-600 rounded-md hover:bg-blue-500">Add to Cart</button>
-                </div>
-            </div>
-            <div className="duration-300 scale-95 border border-gray-100 rounded-lg shadow-lg hover:scale-100 w-80 h-96 hover:shadow-xl">
-                <h1 className="mt-2 text-2xl">MacBook Pro 13"</h1>
-                <img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000" alt="" />
-                <div className="flex items-center justify-between px-12">
-                    <h4>$5555</h4>
-                    <button className="p-2 text-white bg-blue-600 rounded-md hover:bg-blue-500">Add to Cart</button>
-                </div>
-            </div>
-
-
-
+        <div className="grid grid-cols-1 gap-10 py-4 text-center lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2">
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
         </div>
     )
 }
