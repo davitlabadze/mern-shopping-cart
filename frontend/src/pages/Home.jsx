2ddc937726ce2da1c1ee65fd5862a2671466281d
davitlabadze <labadzedato18@gmail.com>
2022-06-01 13:35:40 +0400

    style: home design

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
     return (
-        <div className="z-20 grid grid-flow-col gap-10 py-4 overflow-x-auto text-center ">
+        <div className="grid grid-cols-1 gap-10 py-4 text-center  lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2">
             <div className="duration-300 scale-95 border border-gray-100 rounded-lg shadow-lg hover:scale-100 w-80 h-96 hover:shadow-xl">
                 <h1 className="mt-2 text-2xl">MacBook Pro 13"</h1>
                 <img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000" alt="" />
