fcd45f95a547c7719aac53501f1d3018901eb139
davitlabadze <labadzedato18@gmail.com>
2022-06-02 21:00:51 +0400

    refactor:cart page functional

--- frontend/src/components/CartItem.jsx
+++ frontend/src/components/CartItem.jsx
@@ -1,23 +1,28 @@
 import { Link } from "react-router-dom"
 import { TrashIcon } from '@heroicons/react/outline'
 
-const CartItem = () => {
+const CartItem = ({ item, qtyChangeHandler, removeHandler }) => {
     return (
         <div className="flex items-center justify-between w-full p-4 shadow-md">
-            <img className="w-24 h-24" src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000" alt="" />
-            <Link to={`/product/${111}`}>
-                <p>Product 1</p>
+            <img
+                className="w-24 h-24"
+                src={item.imageUrl}
+                alt={item.name}
+            />
+            <Link to={`/product/${item.product}`}>
+                <p>{item.name}</p>
 
             </Link>
-            <p>$5555</p>
-            <select className="w-12 h-8">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
+            <p>${item.price}</p>
+            <select className="w-12 h-8"
+                value={item.qty}
+                onChange={(e) => qtyChangeHandler(item.product, e.target.value)}>
+                {[...Array(item.countInStock).keys()].map(x => (
+
+                    <option key={x + 1} value={x + 1}>{x + 1}</option>
+                ))}
             </select>
-            <button>
+            <button onClick={() => removeHandler(item.product)}>
                 <TrashIcon className="w-6 h-6 text-gray-500 hover:text-gray-600" />
             </button>
         </div>
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,14 +1,42 @@
+import { useDispatch, useSelector } from "react-redux"
+import { Link } from "react-router-dom";
 import CartItem from "../components/CartItem"
-
+import { addToCart, removeFromCart } from "../redux/actions/cartAction"
 const Cart = () => {
+    const dispatch = useDispatch();
+
+    const cart = useSelector(state => state.cart);
+    console.log(cart)
+    const { cartItems = [] } = cart;
+    console.log(cartItems)
+
+    const qtyChangeHandler = (id, qty) => {
+        dispatch(addToCart(id, qty));
+    }
+
+    const removeHandler = (id) => {
+        dispatch(removeFromCart(id))
+    }
+
+
     return (
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 md:grid-cols-1 sm:grid-cols-1">
             <div className="col-span-2">
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
-            </div>
+                {cartItems.length === 0 ? (
+                    <div>
+                        Your cart is empty <Link to="/" className="text-blue-500" >Go Back</Link>
+                    </div>
+                ) :
+                    cartItems.map((item) => (
+                        <CartItem
+                            item={item}
+                            qtyChangeHandler={qtyChangeHandler}
+                            removeHandler={removeHandler}
+                        />
+                    ))
+                }
+
+            </div >
             <div className="w-full h-40 shadow-md">
                 <div className="w-full p-4 border-b-2 ">
                     <p>Subtotal (0) items</p>
@@ -18,7 +46,7 @@ const Cart = () => {
                     <button className="p-4 mt-2 mb-2 text-center text-white bg-blue-600 hover:bg-blue-500">Process to Checkout</button>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
