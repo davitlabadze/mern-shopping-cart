f570c56c05064d3cb647c5209da73fabb964a807
davitlabadze <labadzedato18@gmail.com>
2022-06-04 15:52:26 +0400

    refactor(product): add more info

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,25 +1,61 @@
 const products = [
     {
-        name: "MacBook Pro 13-inch",
+        name: "MacBook Air",
+        description:"MacBook Air",
+        price:999,
+        countInStock:15,
+        imageUrl:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-air-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726242000",
+    },
+    {
+        name: "MacBook Pro 13",
         description:"MacBook Pro 13-inch",
         price:1299,
         countInStock:12,
         imageUrl:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000",
     },
     {
-        name: "MacBook Pro 14-inch",
+        name: "MacBook Pro 14",
         description:"MacBook Pro 14-inch",
-        price:2299,
+        price:1999,
         countInStock:15,
-        imageUrl:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000",
+        imageUrl:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-14-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000",
     },
     {
-        name: "MacBook Pro 15-inch",
-        description:"MacBook Pro 15-inch",
-        price:3299,
+        name: "MacBook Pro 16",
+        description:"MacBook Pro 16-inch",
+        price:2499,
         countInStock:15,
-        imageUrl:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-13-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726243000",
+        imageUrl:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-16-202110?wid=600&hei=500&fmt=p-jpg&qlt=95&.v=1633726244000",
     },
+    {
+        name: "IMac 24",
+        description:"MacBook Pro 15-inch",
+        price:1299,
+        countInStock:20,
+        imageUrl:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-imac-24-202108?wid=490&hei=400&fmt=jpeg&qlt=95&.v=1625868688000",
+    },
+    {
+        name: "iPhone 13 Pro",
+        description:"iPhone 13 Pro",
+        price:999,
+        countInStock:7,
+        imageUrl:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone13pro-202203?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1644989935267",
+    },
+    {
+        name: "iPhone 13",
+        description:"iPhone 13",
+        price:999,
+        countInStock:8,
+        imageUrl:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone13-202203?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1646335268199",
+    },
+    {
+        name: "iPhone 12",
+        description:"iPhone 12",
+        price:599,
+        countInStock:5,
+        imageUrl:"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone12purple-202109?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1646335268265",
+    },
+   
 ];
 
 module.exports = products;
