f62336b22e683013cf9509a73b615ff33bef7cac
davitlabadze <labadzedato18@gmail.com>
2022-06-04 15:51:22 +0400

    fix(product): image size

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -14,9 +14,11 @@ const Product = ({ imageUrl, name, price, productId }) => {
     return (
         <div className="-ml-4 duration-300 scale-95 border border-gray-100 rounded-lg shadow-lg hover:scale-100 w-80 h-96 hover:shadow-xl">
             <h1 className="mt-2 text-2xl">{name}</h1>
-            <Link to={`/product/${productId}`}>
-                <img src={imageUrl} alt={name} />
-            </Link>
+            <div className='h-3/4'>
+                <Link to={`/product/${productId}`}>
+                    <img src={imageUrl} alt={name} />
+                </Link>
+            </div>
             <div className="flex items-center justify-between px-12">
                 <h4>${price}</h4>
                 <button onClick={addToCartHandler} className="p-2 text-white bg-blue-600 rounded-md hover:bg-blue-500">Add to Cart</button>
